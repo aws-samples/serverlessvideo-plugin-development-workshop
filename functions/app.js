1818a64a05d8ed365645cd64de59393a14ed5aba
Alexander Schueren <sha@amazon.com>
2023-11-09 09:27:16 +0100

    init

---
+++ functions/app.js
@@ -0,0 +1,15 @@
+exports.lambdaHandler = async (event, context) => {
+  // TOOD: implement
+};
+
+const downloadVideo = async (video) => {
+  // TOOD: implement
+}
+
+const transcribeVideo = async (video) => {
+  // TOOD: implement
+}
+
+const sendPluginResult = async (transcribeJobName, s3OutputUri, taskToken) => {
+  // TOOD: implement
+}
