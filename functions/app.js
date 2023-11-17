484d87fdca6ff7803ff3a41969811b7b6470aca7
Alexander Schueren <sha@amazon.com>
2023-11-17 12:05:40 +0100

    fix todo

--- functions/app.js
+++ functions/app.js
@@ -1,15 +1,15 @@
 exports.lambdaHandler = async (event, context) => {
-  // TOOD: implement
+  // TODO: implement
 };
 
 const downloadVideo = async (video) => {
-  // TOOD: implement
+  // TODO: implement
 }
 
 const transcribeVideo = async (video) => {
-  // TOOD: implement
+  // TODO: implement
 }
 
 const sendPluginResult = async (transcribeJobName, s3OutputUri, taskToken) => {
-  // TOOD: implement
+  // TODO: implement
 }
